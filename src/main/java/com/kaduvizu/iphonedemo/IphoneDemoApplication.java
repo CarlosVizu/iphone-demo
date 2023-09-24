bfb9bee811ea3436006ef67b025427c51cfc5d13
kaduvizu <kaduvizu@hotmail.com>
2023-09-24 01:47:27 -0300

    initial commit

---
+++ src/main/java/com/kaduvizu/iphonedemo/IphoneDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kaduvizu.iphonedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IphoneDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IphoneDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaduvizu/iphonedemo/IphoneDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaduvizu.iphonedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IphoneDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
